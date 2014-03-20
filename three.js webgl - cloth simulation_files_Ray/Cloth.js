3b383fccf8aad7eec1e455f98466ec9093d4cfe3
NSunaga <nobuya.sunaga@gmail.com>
2014-03-20 17:14:37 +0900

    Collision Detection Using Raycastering
    Cloth collision detection using three.js Raycaster

---
+++ three.js webgl - cloth simulation_files_Ray/Cloth.js
@@ -0,0 +1,17 @@
+/*
+ * Cloth Simulation using a relaxed constrains solver
+ */
+
+// Suggested Readings
+
+// Advanced Character Physics by Thomas Jakobsen Character
+// http://freespace.virgin.net/hugo.elias/models/m_cloth.htm
+// http://en.wikipedia.org/wiki/Cloth_modeling
+// http://cg.alexandra.dk/tag/spring-mass-system/
+// Real-time Cloth Animation http://www.darwin3d.com/gamedev/articles/col0599.pdf
+
+
+
+
+
+
